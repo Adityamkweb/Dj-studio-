639588984118c9667c4d1de65f793efad955828c
Adityamkweb <adityaaish143@outlook.com>
2025-12-28 21:12:35 +0530

    Refactor audio playback and add visualization
    Refactor audio playback to use AudioContext and gain nodes for volume control. Implement wave drawing for audio visualization.

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-let A = new Audio(), B = new Audio();
+let ctx = new (window.AudioContext || window.webkitAudioContext)();
 
-function play(id){
-  let file = document.getElementById(id).files[0];
-  if(id=="a"){ A.src=URL.createObjectURL(file); A.play(); }
-  else{ B.src=URL.createObjectURL(file); B.play(); }
+let deckA = new Audio();
+let deckB = new Audio();
+
+let srcA = ctx.createMediaElementSource(deckA);
+let srcB = ctx.createMediaElementSource(deckB);
+
+let gainA = ctx.createGain();
+let gainB = ctx.createGain();
+
+srcA.connect(gainA).connect(ctx.destination);
+srcB.connect(gainB).connect(ctx.destination);
+
+let analyserA = ctx.createAnalyser();
+let analyserB = ctx.createAnalyser();
+
+gainA.connect(analyserA);
+gainB.connect(analyserB);
+
+analyserA.fftSize = 256;
+analyserB.fftSize = 256;
+
+function playDeck(d){
+  ctx.resume();
+  if(d==="A"){
+    deckA.src = URL.createObjectURL(fileA.files[0]);
+    deckA.play();
+    drawWave(analyserA, waveA);
+  }else{
+    deckB.src = URL.createObjectURL(fileB.files[0]);
+    deckB.play();
+    drawWave(analyserB, waveB);
+  }
 }
 
-document.getElementById("mix").oninput=e=>{
+function drawWave(analyser, canvas){
+  let c = canvas.getContext("2d");
+  let data = new Uint8Array(analyser.frequencyBinCount);
+
+  function draw(){
+    requestAnimationFrame(draw);
+    analyser.getByteFrequencyData(data);
+    c.fillStyle="#000";
+    c.fillRect(0,0,canvas.width,canvas.height);
+    let w = canvas.width/data.length;
+    data.forEach((v,i)=>{
+      c.fillStyle="#0ff";
+      c.fillRect(i*w,canvas.height,v/3,w-1);
+    });
+  }
+  draw();
+}
+
+cross.oninput=e=>{
   let v=e.target.value/100;
-  A.volume=1-v;
-  B.volume=v;
-}
+  gainA.gain.value=1-v;
+  gainB.gain.value=v;
+};
